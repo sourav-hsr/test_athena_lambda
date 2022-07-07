0b1a5d6e26da886271deffba849714b4a0e8f880
sourav-hsr <sourav@healthsolutionsresearch.org>
2022-07-07 12:17:40 -0500

    Retry Upload 4

--- numdevfun/app.py
+++ numdevfun/app.py
@@ -15,20 +15,37 @@ from lib.appFunctions import *
 
 # The following is the bare minimum you need for the lambda function to run off of an event
 def handler(event, context):
-    try:
-        print("Received event: " + json.dumps(event))
-        # This if block handles the formatting that happens when it is invoked by an event from event bridge
-        if 'Detail' in event:
-            event_payload = event['Detail']
-        elif 'detail' in event:
-            event_payload = event['detail']
-        else:
-            event_payload = event
-        
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Success')
+    DATABASE = 'stg-authdatadb'
+    TABLE='sgcbgcensus'
+    TABLE2='home_panel_summary'
+    output='s3://stg-hsr-athena/bri-refactor/Sourav_test_athena/docker_github_actions_results/'
+
+
+    STATE=event['state'].lower()
+    DATE=event['date']
+
+    print(STATE)
+    print(DATE)
+
+    #query = """ SELECT * FROM "stg-authdatadb"."sgcbgcensus" """
+    query=("SELECT b.number_devices_residing,"
+    "b.census_block_group,"
+    "c.b01001e1, "
+    "b.date_range_start "
+    f"""FROM "{DATABASE}"."{TABLE}" c """
+    f"""LEFT OUTER JOIN "{DATABASE}"."{TABLE2}" b ON lpad(replace(c.census_block_group, '.0'), 12, '0') = lpad(replace(replace(b.census_block_group, 'CA:',''), '.0', ''), 12, '0') """
+    f"""WHERE b.region = '{STATE}' """
+    f"""and lpad(b.date_range_start, 10, '0') = '{DATE}' """)
+    print(query)
+    client = boto3.client('athena')
+    # Execution
+    response = client.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={
+            'Database': DATABASE
+        },
+        ResultConfiguration={
+            'OutputLocation': output,
         }
-    except Exception:
-        print('Failed')
-        traceback.print_exc(Exception)
+    )
+    return response

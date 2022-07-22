6654f2110d39e765d382c743bf0a684350833375
sourav-hsr <sourav@healthsolutionsresearch.org>
2022-07-22 12:45:02 -0500

    Update trigger json

--- testnumdevfun/trigger.py
+++ testnumdevfun/trigger.py
@@ -1,18 +1,28 @@
+import boto3
+import json
+
+
 def trigger():
 
 	# 
 	cloudwatch_events = boto3.client('events')
 
+	print(cloudwatch_events)
+
 	# create payload
 	payload = {
 		  "key1": "value1",
 		  "state": "MD",
-		  "date": "2021-06-28"
+		  "date": "2021-06-28",
+		  "eventType":"testnumdevfun"
 	}
+
+
+	print(payload)
 
 	# send to eventbus
 	full_event = {
-	    "Detail": json.dumps(payload),
+	    "detail": json.dumps(payload),
 	    "DetailType": "testnumdevfun",
 	    "Source": "testnumdevfunEvent",
 	    "EventBusName": "stg-hsr-event-bus"
@@ -20,6 +30,8 @@ def trigger():
 
 
 	event_response = cloudwatch_events.put_events(Entries = [full_event])	
+
+	print(event_response)
 
 	return 0
 
